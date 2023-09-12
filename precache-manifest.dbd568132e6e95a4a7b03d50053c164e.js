93fdad46ef7f7ae4fadb737e2c6b8ecb20d99eb1
Anas Jahangir <anasm8931@gmail.com>
2023-09-13 00:04:18 +0500

    Updates

---
+++ precache-manifest.dbd568132e6e95a4a7b03d50053c164e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ccc72e1d81da8ad09052",
+    "url": "/basic-quiz-app/static/css/main.7621e86d.chunk.css"
+  },
+  {
+    "revision": "ccc72e1d81da8ad09052",
+    "url": "/basic-quiz-app/static/js/main.ccc72e1d.chunk.js"
+  },
+  {
+    "revision": "3fcfe38269dde5cb3eaf",
+    "url": "/basic-quiz-app/static/css/1.5eda6f49.chunk.css"
+  },
+  {
+    "revision": "3fcfe38269dde5cb3eaf",
+    "url": "/basic-quiz-app/static/js/1.3fcfe382.chunk.js"
+  },
+  {
+    "revision": "f443a49f05f3e9a314a7",
+    "url": "/basic-quiz-app/static/js/runtime~main.f443a49f.js"
+  },
+  {
+    "revision": "a0be24ce5fb977856667ea42bc9530d4",
+    "url": "/basic-quiz-app/static/media/250-Control-System-MCQ.a0be24ce.jpg"
+  },
+  {
+    "revision": "986022cce29b90000a58eb9aeb46ddff",
+    "url": "/basic-quiz-app/index.html"
+  }
+];
